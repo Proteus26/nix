9fdc70f58f91e46e8f61e9b4a14556363661290f
Proteus26 <lohithds52@gmail.com>
2026-07-21 20:36:28 +0530

    feat: rearranged the entire config to look cleaner and add some packages

--- configuration.nix
+++ configuration.nix
@@ -1,60 +1,24 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
     ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # Bootloader.
+  # Boot params
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   boot.kernelParams = [ "nvidia-drm.modeset=1" ];
 
-  # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  networking.hostName = "nix"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  virtualisation.docker = {
-    enable = true;
-    enableOnBoot = true;
-
-    rootless = {
-      enable = false;
-    };
-
-    daemon.settings = {
-      features = {
-        buildkit = true;
-      };
-    };
-
-    autoPrune = {
-      enable = true;
-      dates = "weekly";
-    };
-  };
-
-  # Set your time zone.
+  # Locales and other bs
   time.timeZone = "Asia/Kolkata";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
@@ -68,21 +32,25 @@
     LC_TELEPHONE = "en_IN";
     LC_TIME = "en_IN";
   };
-  programs.hyprland = {
-    enable = true;
-    xwayland.enable = true;
+
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
   };
 
+	# Env variables
+	environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+    XDG_SESSION_TYPE = "wayland";
+    AQ_DRM_DEVICES = "/dev/dri/intel-igpu:/dev/dri/nvidia-dgpu";
+		FREETYPE_PROPERTIES = "cff:no-stem-darkening=0 autofitter:no-stem-darkening=0";
+  };
+
+	# Graphics card bs
   services.udev.extraRules = ''
     KERNEL=="card*", KERNELS=="0000:00:02.0", SUBSYSTEM=="drm", SUBSYSTEMS=="pci", SYMLINK+="dri/intel-igpu"
     KERNEL=="card*", KERNELS=="0000:01:00.0", SUBSYSTEM=="drm", SUBSYSTEMS=="pci", SYMLINK+="dri/nvidia-dgpu"
   '';
-
-  environment.sessionVariables = {
-    NIXOS_OZONE_WL = "1";
-    XDG_SESSION_TYPE = "wayland";
-    AQ_DRM_DEVICES = "/dev/dri/intel-igpu:/dev/dri/nvidia-dgpu";
-  };
 
   hardware.graphics = {
     enable = true;
@@ -113,6 +81,84 @@
     };
   };
 
+	# Userslop
+  users.users."proteus" = {
+    isNormalUser = true;
+    description = "proteus";
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+		shell = pkgs.zsh;
+    packages = with pkgs; [];
+  };
+
+  nixpkgs.config.allowUnfree = true;
+  environment.systemPackages = with pkgs; [
+		# Compilers and languages and such
+    gcc
+		go
+		python3
+		nodejs
+		pnpm
+
+		# Dev tools
+    git
+		docker-compose
+    docker-buildx
+    tree-sitter
+		neovim
+		tmux
+		man
+		man-pages
+		man-pages-posix
+
+		# CLI tools
+		wget
+		unzip
+		fzf
+		zoxide
+		eza
+		ripgrep
+		btop
+		glib
+		wireguard-tools
+    jq
+    playerctl
+		direnv
+
+		# WM / Desktop related
+		hyprpaper
+		hyprlock
+		hyprshot
+		quickshell
+    wl-clipboard
+    cliphist
+    libnotify
+    nwg-look
+    libsForQt5.qt5ct
+    kdePackages.qt6ct
+    libsForQt5.qtstyleplugin-kvantum
+    kdePackages.qtstyleplugin-kvantum
+    dconf
+
+		#Apps
+    zsh
+    kitty
+    firefox
+    vesktop
+    yazi
+    nautilus
+    file-roller
+    qbittorrent
+    mpv
+		ghidra
+		onlyoffice-desktopeditors
+  ];
+
+	# Hyprland initialization
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
   xdg.portal = {
     enable = true;
     extraPortals = with pkgs; [
@@ -130,76 +176,29 @@
     };
   };
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
+	# Program Enabling
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
   };
 
-  security.rtkit.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users."proteus" = {
-    isNormalUser = true;
-    description = "proteus";
-    extraGroups = [ "networkmanager" "wheel" "docker" ];
-		shell = pkgs.zsh;
-    packages = with pkgs; [];
+  programs.appimage = {
+    enable = true;
+    binfmt = true;
   };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
+  programs.dconf.enable = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    gcc
-    tree-sitter
-    git
-    wget
-    unzip
-    zsh
-    neovim
-    kitty
-    hyprpaper
-    hyprlock
-    hyprshot
-    quickshell
-    firefox
-    vesktop
-    fzf
-    zoxide
-    eza
-    ripgrep
-    wireguard-tools
-    btop
-    wl-clipboard
-    cliphist
-    yazi
-    nautilus
-    file-roller
-    docker-compose
-    docker-buildx
-    nwg-look
-    libsForQt5.qt5ct
-    kdePackages.qt6ct
-    libsForQt5.qtstyleplugin-kvantum
-    kdePackages.qtstyleplugin-kvantum
-    qbittorrent
-    python3
-    mpv
-    glib
-    dconf
-    jq
-    playerctl
-    libnotify
-    nitch
-  ];
+	programs.zsh.enable = true;
 
+	# For some poop
   programs.nix-ld.enable = true;
   programs.nix-ld.libraries = with pkgs; [
   ];
 
+	# Fontslop
   fonts = {
     packages = with pkgs; [
       inter
@@ -220,26 +219,13 @@
         serif     = [ "Liberation Serif" ];
         emoji     = [ "Noto Color Emoji" ];
       };
+			hinting = {
+				enable = true;
+				style = "slight";
+			};
+			antialias = true;
     };
   };
-
-  programs.appimage = {
-    enable = true;
-    binfmt = true;
-  };
-
-  services.flatpak.enable = true;
-
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = true;
-    localNetworkGameTransfers.openFirewall = true;
-  };
-
-  programs.dconf.enable = true;
-
-	programs.zsh.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -249,16 +235,16 @@
   #   enableSSHSupport = true;
   # };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
+  # Services 
   services.openssh.enable = true;
+
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
   };
+
   services.tlp = {
     enable = true;
     settings = {
@@ -269,17 +255,7 @@
     };
   };
 
-  networking.extraHosts = ''
-    172.16.91.124  hpc01.sharanga.local hpc01
-    172.16.91.125  hpc02.sharanga.local hpc02
-  '';
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  hardware.bluetooth = {
+	hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
   };
@@ -288,6 +264,52 @@
   '';
   services.blueman.enable = true;
 
+	services.printing.enable = true;
+
+  services.flatpak.enable = true;
+
+	security.rtkit.enable = true;
+
+	#Networking
+  networking.extraHosts = ''
+    172.16.91.124  hpc01.sharanga.local hpc01
+    172.16.91.125  hpc02.sharanga.local hpc02
+  '';
+
+  networking.hostName = "nix";
+
+	networking.networkmanager.enable = true;
+
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+	# Virtualisation
+  virtualisation.docker = {
+    enable = true;
+    enableOnBoot = true;
+
+    rootless = {
+      enable = false;
+    };
+
+    daemon.settings = {
+      features = {
+        buildkit = true;
+      };
+    };
+
+    autoPrune = {
+      enable = true;
+      dates = "weekly";
+    };
+  };
+
+	# Filesystem related things
   fileSystems."/mnt/ssdihh" = {
   device = "/dev/disk/by-uuid/7548a1ed-3d14-4e48-b512-baa41300e8ec";
   fsType = "ext4";
@@ -300,12 +322,5 @@
     options = "--delete-older-than 7d";
   };
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "26.05"; # Did you read the comment?
-
+  system.stateVersion = "26.05";
 }
