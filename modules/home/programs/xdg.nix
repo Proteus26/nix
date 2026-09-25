36727ee87ad80a85632878222af12ac4c0478f68
Proteus26 <lohithds52@gmail.com>
2026-09-26 02:15:15 +0530

    fix: other folders spawning on login

--- modules/home/programs/xdg.nix
+++ modules/home/programs/xdg.nix
@@ -13,6 +13,13 @@ in
         enable = true;
         createDirectories = true;
         download = "${config.home.homeDirectory}/downloads";
+        desktop = "${config.home.homeDirectory}";
+        documents = "${config.home.homeDirectory}";
+        music = "${config.home.homeDirectory}";
+        pictures = "${config.home.homeDirectory}";
+        publicShare = "${config.home.homeDirectory}";
+        templates = "${config.home.homeDirectory}";
+        videos = "${config.home.homeDirectory}";
       };
     };
   };
